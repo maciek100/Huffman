d7c770ef4fae85ecfe9799be5bcdf594a4de32b7
maciek100 <maciek100@gmail.com>
2020-05-20 12:33:54 -0500

    Update NodeTest.java

--- src/tests/huffman/NodeTest.java
+++ src/tests/huffman/NodeTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class NodeTest {
+    // TODO : this 'test' ain't doing much ...
     @Test
     public void testTreeBuilding() {
         assertTrue("", true);
